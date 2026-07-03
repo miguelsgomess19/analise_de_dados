a8535a051acdf257dfbf41fc17d6dc5179ffb7f7
miguelsgomess19 <miguelsgomess13@gmail.com>
2026-07-03 20:37:24 -0300

    ia

---
+++ app.py
@@ -0,0 +1,8 @@
+#Comando para exibir informações na tela 
+print("Boa noite!")
+idade = 30 
+idade_dois = "30"
+idade_tres = 30.5
+print(type(idade))
+print(type(idade_dois))
+print(type(idade_tres))
